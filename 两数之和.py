b61c06cb6110879d81069dad147b66d6c65f94cf
mayuying <2995876255@qq.com>
2024-12-02 14:18:10 +0800

    两数之和

--- 两数之和.py
+++ 两数之和.py
@@ -1,1 +1,10 @@
-print(a)
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] +nums[j] == target:
+                    return i,j
+        
+        
