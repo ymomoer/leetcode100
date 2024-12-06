ba338c292aa0dcd34e6c7e74f4b68f40a412a54c
mayuying <2995876255@qq.com>
2024-12-06 18:16:52 +0800

    有序数组的平方

---
+++ 数组/977_有序数组的平方/sortedSquares2.java
@@ -0,0 +1,34 @@
+// 双指针
+import java.util.Arrays;
+
+class sortedSquares2{
+    public int[] sortedSquares(int[] nums){
+        int[] result = new int[nums.length];
+        int k = result.length-1;
+        for(int i = 0,j=nums.length-1;i<=j;){
+            if(nums[i]*nums[i]<nums[j]*nums[j]){
+                result[k] = nums[j]*nums[j];
+                k--;
+                j--;
+            }
+            else{
+                
+                result[k] = nums[i]*nums[i];
+                k--;
+                i++;
+                
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        sortedSquares2 solution = new sortedSquares2();
+        
+        int[] nums={-7,-3,2,3,11};
+        int[] result1 = solution.sortedSquares(nums);
+        System.out.println("Output: "+Arrays.toString(result1));
+
+    }
+}
+
