25bcb2e0ba76a9e5bacae0318554c51de4ee37fc
mayuying <2995876255@qq.com>
2024-12-09 14:58:38 +0800

    反转链表

---
+++ 链表/206_反转链表/reverseList1.java
@@ -0,0 +1,52 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
+public class reverseList1 {
+    public ListNode reverseList(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur!=null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        head.next.next.next.next=new ListNode(5);
+
+        System.out.print("Original List:");
+        printList(head);
+
+        reverseList1 reverser = new reverseList1();
+        ListNode reversedhead = reverser.reverseList(head);
+
+        System.out.print("Reversed List:");
+        printList(reversedhead);
+    }
+
+    public static void printList(ListNode head){
+        ListNode curr = head;
+        while (curr!=null) {
+            System.out.print(curr.val+" ");
+            curr = curr.next;
+            
+        }
+        System.out.println();
+    }
+}
