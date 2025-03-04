a3e5c2b11bd04a0d45de38186539cb3efbefcbaf
mayuying <2995876255@qq.com>
2025-03-04 21:16:08 +0800

    二叉树的层次遍历

---
+++ src/二叉树/107_二叉树的层次遍历II/levelOrder2.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+   TreeNode left;
+   TreeNode right;
+   TreeNode() {}
+   TreeNode(int val) { this.val = val; }
+   TreeNode(int val, TreeNode left, TreeNode right) {
+       this.val = val;
+       this.left = left;
+       this.right = right;
+    }
+}
+
+public class levelOrder2 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root!=null){
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        // 创建二叉树
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+       
+        levelOrder2 solution = new levelOrder2();
+
+       List<List<Integer>> result = solution.levelOrder(root);
+
+        System.out.println("中序遍历结果: " + result);
+    }
+}
