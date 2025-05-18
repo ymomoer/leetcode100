a184cd278dc9565aed68ec4c0f764a32253dcebd
mayuying <2995876255@qq.com>
2025-05-18 13:07:36 +0800

    整数拆分

---
+++ src/动态规划/343_整数拆分/integerBreak1.java
@@ -0,0 +1,12 @@
+public class integerBreak1 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<=i-j;j++){
+                dp[i] = Math.max(dp[i],Math.max(j*(i-j),j*dp[i-j]));
+            }
+        }
+        return dp[n];
+    }
+}
